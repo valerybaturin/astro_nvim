13e8065e2b60c75adc24240c450bdbd445e2c77b
Valery Baturin <Valery.Baturin@cyberprotect.ru>
2023-05-15 15:53:28 +0300

    Change theme to darkplus

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "catppuccin",
+  colorscheme = "darkplus",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/user.lua
+++ plugins/user.lua
@@ -22,5 +22,8 @@ return {
     event = { "CmdlineEnter" },
     ft = { "go", 'gomod' },
     build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
+  {
+    "lunarvim/darkplus.nvim",
   }
 }
