0fc955968ca3cd6452e810f1e89b430dbaa9633b
Valery Baturin <Valery.Baturin@cyberprotect.ru>
2023-07-24 10:23:19 +0300

    Remove catppuccin

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,15 +1,15 @@
 return {
   -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
+  -- "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  {
-    "catppuccin",
-    opts = {
-      flavour = "macchiato", -- latte, frappe, macchiato, mocha
-    }
-  },
+  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  -- {
+  --   "catppuccin",
+  --   opts = {
+  --     flavour = "macchiato", -- latte, frappe, macchiato, mocha
+  --   }
+  -- },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
